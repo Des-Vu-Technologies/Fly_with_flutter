65d9fe69d7dc0c652bab689ab6155026f7f76b1e
shankarkharel <59717384+shankarkharel@users.noreply.github.com>
2023-10-18 19:32:48 +0545

    debugging

---
+++ debugging a fluttter app/debugging_example/android/app/src/main/kotlin/com/example/debugging_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.debugging_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ debugging a fluttter app/debugging_example/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int number = 42; // Introduce a variable to inspect
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Debug Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$number', // Display the variable value
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _incrementCounter(number); // Call a function
+          },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+
+  void _incrementCounter(int num) {
+    
+    num++; // Modify the variable
+    // ignore: avoid_print
+    print('Incrementing: $num'); // Log the value
+  }
+}
