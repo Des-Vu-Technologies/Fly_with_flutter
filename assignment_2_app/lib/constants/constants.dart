53e034ee26b9cad159aa7f3eba8ed0793f33aa73
shankarkharel <59717384+shankarkharel@users.noreply.github.com>
2023-10-24 06:47:30 +0545

    constants

---
+++ assignment_2_app/lib/constants/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color workColor = Colors.blue;
+
+const Color financeColor = Colors.purple;
+
+const Color personalColor = Colors.pink;
+
+const Color studyColor = Colors.yellow;
+
+const Color isDoneColor = Color.fromARGB(255, 255, 0, 0);
