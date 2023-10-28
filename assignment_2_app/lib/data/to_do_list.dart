fb91ce4977f8f3ffd704528b3952c2dcd584b74a
shankarkharel <59717384+shankarkharel@users.noreply.github.com>
2023-10-28 11:07:42 +0545

    added map implementation

--- assignment_2_app/lib/data/to_do_list.dart
+++ assignment_2_app/lib/data/to_do_list.dart
@@ -1,1 +1,4 @@
-List<Map<String, dynamic>> toDoTasks = [];
+List<Map<String, dynamic>> toDoTasks = [
+  {"task": "repair car", "isDone": false, "category": "finance"},
+  {"task": "learn Node", "isDone": true, "category": "study","dueDate":"2023:01:02"},
+];
